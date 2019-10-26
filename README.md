759e62db35d8de6d7462f299b19f1bfddd1c6011
kamiloC <kamilo.sniper@gmail.com>
2019-10-25 23:18:52 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# smart-lift-ts
-Created with CodeSandbox
---
+++ src/components/lisft.ts
@@ -0,0 +1,9 @@
+export default class Lift {
+  floorList: Array[number] = [];
+
+  public getCurrentFloor(): number {
+    return this.floorList[0];
+  }
+
+  
+}
